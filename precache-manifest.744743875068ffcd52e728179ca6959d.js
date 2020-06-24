8b2c332ecc1c8990dfd80f5f8a75ce47375dda84
tom temkin <tomtemkin14@gmail.com>
2020-06-24 11:53:17 +0300

    'deploy

---
+++ precache-manifest.744743875068ffcd52e728179ca6959d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4feaeb5592cfdea52b9e",
+    "url": "/dor-swisa/css/app.09966dbf.css"
+  },
+  {
+    "revision": "14316e940f75647665a91af81a15d4c5",
+    "url": "/dor-swisa/index.html"
+  },
+  {
+    "revision": "4feaeb5592cfdea52b9e",
+    "url": "/dor-swisa/js/app.244ae292.js"
+  },
+  {
+    "revision": "09908c039cda853431f3",
+    "url": "/dor-swisa/js/chunk-vendors.faa1b096.js"
+  },
+  {
+    "revision": "f84e7af7f63f4d95ad48a30099dc57cc",
+    "url": "/dor-swisa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dor-swisa/robots.txt"
+  }
+]);
